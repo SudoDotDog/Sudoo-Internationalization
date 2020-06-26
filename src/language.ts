de31785270ccadaf7ca59d114d8c75b5b34082bb
WMXPY <wm@mengw.io>
2020-06-26 09:21:11 -0500

    add more language default support

--- src/language.ts
+++ src/language.ts
@@ -10,14 +10,16 @@ export const getLanguageLocale = (language: string): LOCALE | undefined => {
 
     const lowerCased: string = language.toLowerCase();
     switch (lowerCased) {
-        case 'en-gb':
-            return LOCALE.ENGLISH_UNITED_KINGDOM;
-        case 'en-us':
-            return LOCALE.ENGLISH_UNITED_STATES;
         case 'zh-cn':
             return LOCALE.CHINESE_SIMPLIFIED;
         case 'zh-tw':
             return LOCALE.CHINESE_TRADITIONAL;
+        case 'en-gb':
+            return LOCALE.ENGLISH_UNITED_KINGDOM;
+        case 'en-us':
+            return LOCALE.ENGLISH_UNITED_STATES;
+        case 'en-ca':
+            return LOCALE.ENGLISH_CANADA;
         case 'es-mx':
             return LOCALE.SPANISH_MEXICO;
         case 'fr-ca':
@@ -26,22 +28,22 @@ export const getLanguageLocale = (language: string): LOCALE | undefined => {
 
     const languageHeader: string = lowerCased.substring(0, 2);
     switch (languageHeader) {
-        case 'en':
-            return LOCALE.ENGLISH_UNITED_STATES;
-        case 'es':
-            return LOCALE.SPANISH_SPAIN;
-        case 'ja':
-            return LOCALE.JAPANESE_JAPAN;
         case 'zh':
             return LOCALE.CHINESE_SIMPLIFIED;
+        case 'en':
+            return LOCALE.ENGLISH_UNITED_STATES;
         case 'fr':
             return LOCALE.FRENCH_FRANCE;
+        case 'ja':
+            return LOCALE.JAPANESE_JAPAN;
         case 'ko':
             return LOCALE.KOREAN_KOREA;
         case 'mi':
             return LOCALE.MAORI_NEW_ZEALAND;
         case 'ru':
             return LOCALE.RUSSIAN_RUSSIA;
+        case 'es':
+            return LOCALE.SPANISH_SPAIN;
     }
 
     return undefined;
