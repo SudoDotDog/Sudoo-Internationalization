e6aad8ebf3171007f4fc7fa35f39da51b2cf9093
WMXPY <wm@mengw.io>
2021-06-17 16:27:36 -0500

    use formatted format

--- src/format.ts
+++ src/format.ts
@@ -7,17 +7,17 @@
 import { PROFILE } from "./declare";
 import { fillMessage } from "./util";
 
-export class SudooFormat {
+export class SudooFormat<P extends PROFILE = any> {
 
-    public static create(profile: PROFILE, initial: PROFILE): SudooFormat {
+    public static create<P extends PROFILE = any>(profile: P, initial: P): SudooFormat<P> {
 
         return new SudooFormat(profile, initial);
     }
 
-    private readonly _profile: PROFILE;
-    private readonly _initial: PROFILE;
+    private readonly _profile: P;
+    private readonly _initial: P;
 
-    private constructor(profile: PROFILE, initial: PROFILE) {
+    private constructor(profile: P, initial: P) {
 
         this._profile = profile;
         this._initial = initial;
@@ -27,7 +27,7 @@ export class SudooFormat {
         return Object.keys(this._profile).length;
     }
 
-    public raw(key: string): string {
+    public raw(key: keyof P): string {
 
         if (typeof this._profile[key] === 'string') {
             return this._profile[key];
@@ -35,10 +35,12 @@ export class SudooFormat {
         if (typeof this._initial[key] === 'string') {
             return this._initial[key];
         }
-        throw new Error("[Sudoo-Internationalization] undefined key: {" + key + "}");
+
+        const errorKey: string = key as string;
+        throw new Error(`[Sudoo-Internationalization] undefined key: {${errorKey}}`);
     }
 
-    public get(key: string, ...args: any[]): string {
+    public get(key: keyof P, ...args: any[]): string {
 
         const raw: string = this.raw(key);
         return fillMessage(raw, ...args);
