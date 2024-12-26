7fba0c93ec9fa47fb20a08a63970302d5937811b
wmxpy <wm@mengw.io>
2024-12-26 14:18:07 -0600

    Update .gitignore to remove 'mock' entry and streamline ignored files

---
+++ test/mock/mock-profile.ts
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @namespace Mock
+ * @description Mock Profile
+ */
+
+import { PROFILE } from "../../src/declare";
+
+const mockProfile: PROFILE<string> = {
+    "key1": "value1",
+    "key2": "value2",
+};
+export default mockProfile;
